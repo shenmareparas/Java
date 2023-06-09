da8f03a091e617821ecfe62e686c1f341c90373e
Paras Shenmare <shenmareparas@gmail.com>
2023-06-10 05:09:38 +0530

    .

--- star_pyramid.java
+++ star_pyramid.java
@@ -1,13 +1,17 @@
 import java.util.Scanner;
 
-public class star_pyramid {
-    static void main(String[] args) {
+class star_pyramid {
+    public static void main(String[] args) {
         System.out.println("EnterNumber: ");
         Scanner nc =new Scanner(System.in);
         int num = nc.nextInt();
-
-        for (int i =0;)
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    nc.close();
     }
 }
-
 
