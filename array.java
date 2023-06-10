8f83d1fcbe5cb7669c1f75507ed758c5aa12d045
Paras Shenmare <shenmareparas@gmail.com>
2023-06-10 23:18:46 +0530

    Array

---
+++ array.java
@@ -0,0 +1,11 @@
+class array {
+    public static void main(String[] args) {
+        int mark []= new int[3];
+        mark[0] = 69;
+        mark[1] = 96;
+        mark[2] = 100;
+        for (int i = 0; i < 3; i++) {
+        System.out.println(mark[i]);
+        }
+    }
+}
