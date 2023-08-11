bb1093db087bfa219bc8423bdb23b8a76da995ff
Paras Shenmare <shenmareparas@gmail.com>
2023-08-12 01:26:11 +0530

    Create bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,42 @@
+public class bubblesort {
+
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 64, 34, -25, 12, -22, 11, -90 };
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("\nSorted array:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
